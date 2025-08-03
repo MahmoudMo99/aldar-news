b6545968aac634bcd52e77f92b80e9b9887b970d
Mahmoud Mo <mmahmoud092002@gmail.com>
2025-08-03 14:06:19 +0300

    some works

--- src/app/pages/add-news/add-news.ts
+++ src/app/pages/add-news/add-news.ts
@@ -1,17 +1,56 @@
 import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { QuillModule } from 'ngx-quill';
+
 @Component({
   selector: 'app-add-news',
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, ReactiveFormsModule, QuillModule],
   templateUrl: './add-news.html',
   styleUrl: './add-news.scss',
 })
 export class AddNews {
-  constructor() {}
   router = inject(Router);
+  fb = inject(FormBuilder);
+
+  newsForm = this.fb.group({
+    title: ['', Validators.required],
+    description: ['', Validators.required],
+    image: [null],
+  });
+
+  selectedImage: File | null = null;
+
+  quillConfig = {
+    toolbar: [
+      ['bold', 'italic', 'underline', 'strike'],
+      [{ header: 1 }, { header: 2 }],
+      [{ list: 'ordered' }, { list: 'bullet' }],
+      [{ align: [] }],
+      ['link', 'image', 'video'],
+      ['clean'],
+    ],
+  };
+
+  onFileChange(event: any) {
+    const file = event.target.files[0];
+    if (file) {
+      this.selectedImage = file;
+      this.newsForm.patchValue({ image: file });
+    }
+  }
 
   addNews() {
-    this.router.navigate(['dashboard']);
+    if (this.newsForm.invalid) return;
+
+    const formData = new FormData();
+    formData.append('title', this.newsForm.value.title!);
+    formData.append('description', this.newsForm.value.description!);
+    if (this.selectedImage) {
+      formData.append('image', this.selectedImage);
+    }
+
+    console.log('FormData ready:', formData);
   }
 }
